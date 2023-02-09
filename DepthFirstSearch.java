27579917e4d7a17b31526758d76fcfcdf15c3d5c
Nelson Lu <nelsonlu3@gmail.com>
2023-02-08 21:41:27 -0600

    Update DepthFirstSearch.java

---
+++ DepthFirstSearch.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; // For inputs
+
+public class DepthFirstSearch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); // New Scanner object
+        int numInstances = in.nextInt(); // Reading user input of number of instances
+
+        in.nextLine(); // Clearing \n
+
+        
+    }
+}
