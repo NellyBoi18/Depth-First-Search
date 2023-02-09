763069b1174c80b170d24bbdd6b9f369a237a2dc
Nelson Lu <nelsonlu3@gmail.com>
2023-02-08 21:57:12 -0600

    Update DepthFirstSearch.java

--- DepthFirstSearch.java
+++ DepthFirstSearch.java
@@ -3,8 +3,13 @@ import java.util.Scanner; // For inputs
 public class DepthFirstSearch {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in); // New Scanner object
-        int numInstances = in.nextInt(); // Reading user input of number of instances
 
+        int numInstances = in.nextInt(); // Reading user input of number of instances
+        in.nextLine(); // Clearing \n
+
+        Instances[] instances = new Instances[numInstances]; // Creating an array of objects based on input num of instances
+
+        int numNodes = in.nextInt(); // Reading user input of number of nodes
         in.nextLine(); // Clearing \n
 
         
